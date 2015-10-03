7ccb796b587005a3aa66ccba856713759f99f231
Mike Williams <mdub@dogbiscuit.org>
2015-10-03 21:21:05 +1000

    Get test-bucket name from environment.

--- spec/fake-aws/s3/resource_spec_spec.rb
+++ spec/fake-aws/s3/resource_spec_spec.rb
@@ -3,12 +3,13 @@ require "aws-sdk-resources"
 
 require_relative "resource_behaviour.rb"
 
-if ENV.key?("AWS_ACCESS_KEY_ID")
+if ENV.key?("FAKE_AWS_SDK_TEST_BUCKET")
+
   describe Aws::S3::Resource, :integration => true do
 
     let(:s3) { described_class.new }
 
-    let(:existing_bucket_name) { "fake-aws-sdk-s3-test" }
+    let(:existing_bucket_name) { ENV.fetch("FAKE_AWS_SDK_TEST_BUCKET") }
 
     it_behaves_like "Aws::S3::Resource"
 
